4923f8463de9376919c86b7f73cb22baaeab9b76
almoraboy <68150888+almoraboy@users.noreply.github.com>
2020-07-23 16:36:52 +0530

    Moved code for better readability.

--- stringsequence.h
+++ stringsequence.h
@@ -5,154 +5,19 @@
 
 class StringSequence {
 public:
-    static string longestCommonSubstring( const string& a, const string& b ) {
-        int substrLen = 0, bestEndIndex;
-        string lcs;
-
-        int M = a.size(), N = b.size();
-        vector< vector< int > > lcsMatrix( M + 1, vector< int > ( N + 1, 0 ) );
-        for( int i = 1; i <= M; ++i ) {
-            for( int j = 1; j <= N; ++j ) {
-                lcsMatrix[i][j] = ( a[i-1] == b[j-1] ) ? 1 + lcsMatrix[i-1][j-1] : 0;
-                if( substrLen < lcsMatrix[i][j] ) {
-                    substrLen = lcsMatrix[i][j];
-                    bestEndIndex = i;
-                }
-            }
-        }
-        if( substrLen > 0 )
-            lcs = a.substr( bestEndIndex - substrLen, substrLen );
-        return lcs;
-    }
-    static string longestCommonSubsequence( const string& a, const string& b ) {
-        int M = a.size(), N = b.size();
-        string lcs;
-        vector< vector< int > > lcsMatrix( M + 1, vector< int > ( N + 1, 0 ) );
-        for( int i = 0; i < M; ++i ) {
-            for( int j = 0; j < N; ++j ) {
-                if( a[i] == b[j] )
-                    lcsMatrix[i + 1][j + 1] = 1 + lcsMatrix[i][j];
-                else
-                    lcsMatrix[i + 1][j + 1] = max( lcsMatrix[i][j + 1],
-                                                   lcsMatrix[i + 1][j] );
-            }
-        }
-        int i = M, j = N;
-        while( i + j > 0 ) {
-            if( i > 0 && j > 0 && a[i - 1] == b[j - 1] ) {
-                lcs += b[j - 1]; --i; --j;
-            } else if( j > 0 && lcsMatrix[i][j] == lcsMatrix[i][j - 1] ) {
-                --j;
-            } else {
-                assert( i > 0 );
-                --i;
-            }
-        }
-        reverse( lcs.begin(), lcs.end() );
-        return lcs;
-    }
-    static string shortestCommonSupersequence( const string& a, const string& b ) {
-        int M = a.size(), N = b.size();
-        string scs;
-        vector< vector < int > > scsMatrix( M + 1, vector< int > ( N + 1, 0 ) );
-
-        for( int r = 1; r <= M; ++r ) scsMatrix[r][0] = 1 + scsMatrix[r - 1][0];
-        for( int c = 1; c <= N; ++c ) scsMatrix[0][c] = 1 + scsMatrix[0][c - 1];
-
-        for( int i = 0; i < M; ++i ) {
-            for( int j = 0; j < N; ++j ) {
-                if( a[i] == b[j] )
-                    scsMatrix[i + 1][j + 1] = 1 + scsMatrix[i][j];
-                else
-                    scsMatrix[i + 1][j + 1] = 1 + min( scsMatrix[i][j + 1],
-                                                       scsMatrix[i + 1][j] );
-            }
-        }
-        int i = M, j = N;
-        while( i + j > 0 ) {
-            if( i > 0 && j > 0 && a[i - 1] == b[j - 1] ) {
-                scs += b[j - 1]; --i; --j;
-            } else if( j > 0 && scsMatrix[i][j] == 1 + scsMatrix[i][j - 1] ) {
-                scs += b[j - 1]; --j;
-            } else {
-                assert( i > 0 );
-                scs += a[i - 1]; --i;
-            }
-        }
-        reverse( scs.begin(), scs.end() );
-        return scs;
-    }
+    static string longestCommonSubstring( const string& a, const string& b );
+    static string longestCommonSubsequence( const string& a, const string& b );
+    static string shortestCommonSupersequence( const string& a, const string& b );
     //Is string "c" a shuffle of strings "a" and "b" ?
-    static bool isShuffle( const string& a, const string& b, const string& c ) {
-        int M = a.size(), N = b.size(), K = c.size();
-        if( K != M + N ) return false;
-
-        vector< bool > shuffle( K + 1, false );
-
-        for( int i = 0; i <= M; ++i ) {
-            for( int j = 0; j <= N; ++j ) {
-                if( i + j == 0 ) {
-                    shuffle[ i + j ] = true;
-                    continue;
-                }
-                if( i > 0 && a[i - 1] == c[ i + j - 1 ] )
-                    shuffle[ i + j ] = shuffle[ i + j - 1 ] & true;
-                if( j > 0 && b[j - 1] == c[ i + j - 1 ] )
-                    shuffle[ i + j ] = shuffle[ i + j - 1 ] & true;
-            }
-        }
-        return shuffle[ K ];
-    }
+    static bool isShuffle( const string& a, const string& b, const string& c );
     //A smooth shuffle of "a" and "b" is a shuffle of "a" and "b" that never
     //uses two consecutive symbols of either string. Is "c" a smooth shuffle
     //of "a" and "b" ?
-    static bool isSmoothShuffle( const string& a, const string& b, const string& c ) {
-        int M = a.size(), N = b.size(), K = c.size();
-        if( K != M + N ) return false;
-
-        enum { SHUFFLE_MATCH_NONE = 0x00,
-               SHUFFLE_MATCH_A    = 0x01,
-               SHUFFLE_MATCH_B    = 0x02,
-               SHUFFLE_MATCH_AB   = SHUFFLE_MATCH_A | SHUFFLE_MATCH_B,
-             };
-
-        vector< char > tag( K + 1, SHUFFLE_MATCH_NONE );
-        tag[ 0 ] = SHUFFLE_MATCH_AB;
-
-        for( int i = 0; i <= M; ++i ) {
-            for( int j = 0; j <= N; ++j ) {
-                if( i + j == 0 ) {
-                    continue;
-                }
-                if( i > 0 && a[i - 1] == c[ i + j - 1 ] ) {
-                    tag[ i + j ] |= SHUFFLE_MATCH_A;
-                }
-                if( j > 0 && b[j - 1] == c[ i + j - 1 ] ) {
-                    tag[ i + j ] |= SHUFFLE_MATCH_B;
-                }
-            }
-        }
-        if( tag[ K ] == SHUFFLE_MATCH_NONE ) return false;
-    }
+    static bool isSmoothShuffle( const string& a, const string& b, const string& c );
     //Is string "b" a subsequence of string "a" ?
-    static bool isSubsequence( const string& a, const string& b ) {
-        int M = a.size(), N = b.size();
-        if( N > M ) return false;
+    static bool isSubsequence( const string& a, const string& b );
+    static int longestContiguousForwardsBackwardsSubstring( const string& s );
 
-        auto iter = b.begin();
-        int i = 0;
-
-        while( iter != b.end() && i < M ) {
-            if( a[i++] == *iter ) ++iter;
-        }
-        return iter == b.end();
-    }
-    static int longestContiguousForwardsBackwardsSubstring( const string& s ) {
-        int maxLen = 0;
-        map< ii, int > cache;
-        longestContiguousForwardsBackwardsSubstring_( s, 0, s.size() - 1, cache, maxLen );
-        return maxLen;
-    }
 private:
     static int longestContiguousForwardsBackwardsSubstring_( const string& s, int i, int j,
                                                              map< ii, int >& cache,
@@ -175,6 +40,156 @@ private:
         return len;
     }
 };
+
+string StringSequence::longestCommonSubstring( const string& a, const string& b ) {
+    int substrLen = 0, bestEndIndex;
+    string lcs;
+
+    int M = a.size(), N = b.size();
+    vector< vector< int > > lcsMatrix( M + 1, vector< int > ( N + 1, 0 ) );
+    for( int i = 1; i <= M; ++i ) {
+        for( int j = 1; j <= N; ++j ) {
+            lcsMatrix[i][j] = ( a[i-1] == b[j-1] ) ? 1 + lcsMatrix[i-1][j-1] : 0;
+            if( substrLen < lcsMatrix[i][j] ) {
+                substrLen = lcsMatrix[i][j];
+                bestEndIndex = i;
+            }
+        }
+    }
+    if( substrLen > 0 )
+        lcs = a.substr( bestEndIndex - substrLen, substrLen );
+    return lcs;
+}
+
+string StringSequence::longestCommonSubsequence( const string& a, const string& b ) {
+    int M = a.size(), N = b.size();
+    string lcs;
+    vector< vector< int > > lcsMatrix( M + 1, vector< int > ( N + 1, 0 ) );
+    for( int i = 0; i < M; ++i ) {
+        for( int j = 0; j < N; ++j ) {
+            if( a[i] == b[j] )
+                lcsMatrix[i + 1][j + 1] = 1 + lcsMatrix[i][j];
+            else
+                lcsMatrix[i + 1][j + 1] = max( lcsMatrix[i][j + 1],
+                                               lcsMatrix[i + 1][j] );
+        }
+    }
+    int i = M, j = N;
+    while( i + j > 0 ) {
+        if( i > 0 && j > 0 && a[i - 1] == b[j - 1] ) {
+            lcs += b[j - 1]; --i; --j;
+        } else if( j > 0 && lcsMatrix[i][j] == lcsMatrix[i][j - 1] ) {
+            --j;
+        } else {
+            assert( i > 0 );
+            --i;
+        }
+    }
+    reverse( lcs.begin(), lcs.end() );
+    return lcs;
+}
+
+string StringSequence::shortestCommonSupersequence( const string& a, const string& b ) {
+    int M = a.size(), N = b.size();
+    string scs;
+    vector< vector < int > > scsMatrix( M + 1, vector< int > ( N + 1, 0 ) );
+
+    for( int r = 1; r <= M; ++r ) scsMatrix[r][0] = 1 + scsMatrix[r - 1][0];
+    for( int c = 1; c <= N; ++c ) scsMatrix[0][c] = 1 + scsMatrix[0][c - 1];
+
+    for( int i = 0; i < M; ++i ) {
+        for( int j = 0; j < N; ++j ) {
+            if( a[i] == b[j] )
+                scsMatrix[i + 1][j + 1] = 1 + scsMatrix[i][j];
+            else
+                scsMatrix[i + 1][j + 1] = 1 + min( scsMatrix[i][j + 1],
+                                                   scsMatrix[i + 1][j] );
+        }
+    }
+    int i = M, j = N;
+    while( i + j > 0 ) {
+        if( i > 0 && j > 0 && a[i - 1] == b[j - 1] ) {
+            scs += b[j - 1]; --i; --j;
+        } else if( j > 0 && scsMatrix[i][j] == 1 + scsMatrix[i][j - 1] ) {
+            scs += b[j - 1]; --j;
+        } else {
+            assert( i > 0 );
+            scs += a[i - 1]; --i;
+        }
+    }
+    reverse( scs.begin(), scs.end() );
+    return scs;
+}
+
+bool StringSequence::isShuffle( const string& a, const string& b, const string& c ) {
+    int M = a.size(), N = b.size(), K = c.size();
+    if( K != M + N ) return false;
+
+    vector< bool > shuffle( K + 1, false );
+
+    for( int i = 0; i <= M; ++i ) {
+        for( int j = 0; j <= N; ++j ) {
+            if( i + j == 0 ) {
+                shuffle[ i + j ] = true;
+                continue;
+            }
+            if( i > 0 && a[i - 1] == c[ i + j - 1 ] )
+                shuffle[ i + j ] = shuffle[ i + j - 1 ] & true;
+            if( j > 0 && b[j - 1] == c[ i + j - 1 ] )
+                shuffle[ i + j ] = shuffle[ i + j - 1 ] & true;
+        }
+    }
+    return shuffle[ K ];
+}
+
+bool StringSequence::isSmoothShuffle( const string& a, const string& b, const string& c ) {
+    int M = a.size(), N = b.size(), K = c.size();
+    if( K != M + N ) return false;
+
+    enum { SHUFFLE_MATCH_NONE = 0x00,
+           SHUFFLE_MATCH_A    = 0x01,
+           SHUFFLE_MATCH_B    = 0x02,
+           SHUFFLE_MATCH_AB   = SHUFFLE_MATCH_A | SHUFFLE_MATCH_B,
+         };
+
+    vector< char > tag( K + 1, SHUFFLE_MATCH_NONE );
+    tag[ 0 ] = SHUFFLE_MATCH_AB;
+
+    for( int i = 0; i <= M; ++i ) {
+        for( int j = 0; j <= N; ++j ) {
+            if( i + j == 0 ) {
+                continue;
+            }
+            if( i > 0 && a[i - 1] == c[ i + j - 1 ] ) {
+                tag[ i + j ] |= SHUFFLE_MATCH_A;
+            }
+            if( j > 0 && b[j - 1] == c[ i + j - 1 ] ) {
+                tag[ i + j ] |= SHUFFLE_MATCH_B;
+            }
+        }
+    }
+    if( tag[ K ] == SHUFFLE_MATCH_NONE ) return false;
+}
+
+bool StringSequence::isSubsequence( const string& a, const string& b ) {
+    int M = a.size(), N = b.size();
+    if( N > M ) return false;
+
+    auto iter = b.begin();
+    int i = 0;
+
+    while( iter != b.end() && i < M ) {
+        if( a[i++] == *iter ) ++iter;
+    }
+    return iter == b.end();
+}
+
+int StringSequence::longestContiguousForwardsBackwardsSubstring( const string& s ) {
+    int maxLen = 0;
+    map< ii, int > cache;
+    longestContiguousForwardsBackwardsSubstring_( s, 0, s.size() - 1, cache, maxLen );
+    return maxLen;
+}
 
 class StringSequenceTest {
 public:
